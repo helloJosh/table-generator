2754466228654b11b58c35f3e5f1a5dd7315111f
helloJosh <kimjoshua135@gmail.com>
2024-12-17 20:53:50 +0900

    first commit

---
+++ src/test/java/com/woobeee/tablegenerator/TableGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woobeee.tablegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
